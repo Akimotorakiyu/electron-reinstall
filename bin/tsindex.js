28529871ece81e2fd1b4fa1acb4205f593372688
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-19 12:59:48 +0800

    use new ts/js  way

---
+++ bin/tsindex.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const path = require("path");
+require("ts-node").register({
+  project: path.resolve(__dirname, "..", "tsconfig.json")
+});
+
+// run the CLI with the current process arguments
+require(path.resolve(__dirname, "..", "src", "cli")).default.run();
