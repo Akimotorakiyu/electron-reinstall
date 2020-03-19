4169fedc2d6f030e1ecef7ddbb1d1db6a5d0a343
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-19 11:42:17 +0800

    use --ignore-script command

--- src/commands/electron-reinstall.ts
+++ src/commands/electron-reinstall.ts
@@ -11,6 +11,35 @@ export default <GluegunCommand>{
     const workdir = await toolbox.filesystem.dirAsync("");
     const packageJson = await workdir.readAsync("package.json", "json");
 
+    if (!packageJson) {
+      toolbox.print.warning(
+        "Didn't find `package.json`, is this dir is a JS/TS project? Maybe you should open or create your JS/TS project first?"
+      );
+
+      toolbox.print.info(
+        "- try use `npm init` or `yarn init` to create your JS/TS project."
+      );
+
+      return;
+    }
+
+    if (
+      !(
+        packageJson?.dependencies?.electron ||
+        packageJson?.devDependencies?.electron
+      )
+    ) {
+      toolbox.print.warning(
+        "didn't find electron in dependencies or devDependencies, try to add electron package to your project first!"
+      );
+
+      toolbox.print.info(
+        "- try use `npm install electron -save-dev --ignore-script` or `yarn add electron --dev --ignore-script`."
+      );
+
+      return;
+    }
+
     if (
       packageJson?.dependencies?.electron ||
       packageJson?.devDependencies?.electron
@@ -34,13 +63,6 @@ export default <GluegunCommand>{
       });
       toolbox.print.success(`electron ${result || "installed!"}`);
       await electronDir.removeAsync("newInstall.js");
-    } else {
-      toolbox.print.warning(
-        "didn't find electron in dependencies or devDependencies, add electron package to your project first!"
-      );
-      toolbox.print.warning(
-        "`yarn add -D electron` or `npm install --save-dev electron`."
-      );
     }
   }
 };
