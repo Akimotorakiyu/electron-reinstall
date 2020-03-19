bc2f6cf949f59fda8cae72c90858ceb5e23929cb
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-19 12:42:39 +0800

    use dist

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
-const path = require("path");
-require("ts-node").register({
-  project: path.resolve(__dirname, "..", "tsconfig.json")
-});
-// run the CLI with the current process arguments
-require(path.resolve(__dirname, "..", "src", "cli")).default.run();
+
+require("../dist/cli").default.run();
